04e3f75a5dd5519725083d40020c11a919219f66
Selkia <91175624+Selkia70@users.noreply.github.com>
2022-12-16 12:59:38 +0530

    Update DuplicateLead.java

--- assingments/sel/DuplicateLead.java
+++ assingments/sel/DuplicateLead.java
@@ -16,6 +16,16 @@ public class DuplicateLead {
 		driver.findElement(By.linkText("CRM/SFA")).click();
 		driver.findElement(By.linkText("Leads")).click();
 		driver.findElement(By.linkText("Find Leads")).click();
+		driver.findElement(By.xpath("(//span[@class='x-tab-strip-text '])[3]")).click();
+		driver.findElement(By.xpath("//input[@name='emailAddress']")).sendKeys("johnselsi043@gmail.com");
+		driver.findElement(By.xpath("(//button[@class='x-btn-text'])[6]")).click();
+	    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+
+		driver.findElement(By.xpath("//a[text()='19558']")).click();
+		driver.findElement(By.linkText("Duplicate Lead")).click();
+		driver.findElement(By.id("createLeadForm_generalProfTitle")).sendKeys("Duplicate Lead");
+		driver.findElement(By.className("smallSubmit")).click();
+
 
 	}
 
