ceb8a33c677d9fedab3c0f802c15bacc1892c9ac
Selkia <91175624+Selkia70@users.noreply.github.com>
2022-12-16 12:32:17 +0530

    Update DeleteLead.java

--- assingments/sel/DeleteLead.java
+++ assingments/sel/DeleteLead.java
@@ -16,7 +16,10 @@ public class DeleteLead {
 		driver.findElement(By.linkText("CRM/SFA")).click();
 		driver.findElement(By.linkText("Leads")).click();
 		driver.findElement(By.linkText("Find Leads")).click();
-		//driver.findElement(By.className(" ext-safari")).click();
+		driver.findElement(By.xpath("(//span[@class='x-tab-strip-text '])[2]")).click();
+		driver.findElement(By.xpath("//input[@name='phoneAreaCode']")).sendKeys("91");
+		driver.findElement(By.xpath("//input[@name='phoneNumber']")).sendKeys("6382810309");
+		driver.findElement(By.xpath("(//button[@class='x-btn-text'])[6]")).click();
 
 	}
 
